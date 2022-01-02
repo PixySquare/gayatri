677cafdb4417fa3a6c4f9ef9fdf6c0fe3cea7ab8
avivashishta29 <avivashishta@gmail.com>
2022-01-02 23:22:43 +0530

    about us

--- components/AboutUs/index.js
+++ components/AboutUs/index.js
@@ -1,5 +1,7 @@
 import styles from "./styles.module.css";
-import img2 from "../../public/assets/images/default.png";
+import img2 from "../../public/assets/images/intro2.png";
+import person from "../../public/assets/images/person.png";
+import person2 from "../../public/assets/images/person2.png";
 import Image from "next/image";
 
 function AboutUs() {
@@ -14,7 +16,12 @@ function AboutUs() {
         <div className={styles.innerContainer}>
           <p
             className={styles.lightFont}
-            style={{ marginTop: 6, textAlign: "left", width: "80%", marginRight:"auto" }}
+            style={{
+              marginTop: 6,
+              textAlign: "left",
+              width: "80%",
+              marginRight: "auto",
+            }}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -28,6 +35,53 @@ function AboutUs() {
           </p>
         </div>
         <Image src={img2} alt={"image"} />
+      </div>
+
+      <div className={styles.secondDiv}>
+        <div style={{ textAlign: "left" }}>
+          <Image src={person} />
+          <h4>Name Surname</h4>
+          <p className={styles.lightFont} style={{ marginTop: -20 }}>
+            Position
+          </p>
+        </div>
+
+        <div className={styles.rightDiv}>
+          <p className={styles.lightFont}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Fringilla urna porttitor rhoncus dolor. Ornare lectus sit amet est
+            placerat in egestas. Ultricies lacus sed turpis tincidunt. In nisl
+            nisi scelerisque eu ultrices vitae auctor. Felis imperdiet proin
+            fermentum leo vel orci. Sed odio morbi quis commodo odio aenean.
+            Massa vitae tortor condimentum lacinia quis vel eros donec ac. Sed
+            viverra tellus in hac. Egestas sed tempus urna et. Sed viverra
+            tellus in hac. Egestas sed tempus urna et.
+          </p>
+          <div className={styles.innerFlexDiv}>
+            <div style={{ textAlign: "left" }}>
+              <Image src={person2} height="280px" width="210px" />
+              <h4>Name Surname</h4>
+              <p className={styles.lightFont} style={{ marginTop: -20 }}>
+                Position
+              </p>
+            </div>
+            <div style={{ textAlign: "left" }}>
+              <Image src={person2} height="280px" width="210px" />
+              <h4>Name Surname</h4>
+              <p className={styles.lightFont} style={{ marginTop: -20 }}>
+                Position
+              </p>
+            </div>
+            <div style={{ textAlign: "left" }}>
+              <Image src={person2} height="280px" width="210px" />
+              <h4>Name Surname</h4>
+              <p className={styles.lightFont} style={{ marginTop: -20 }}>
+                Position
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
