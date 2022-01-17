23c50fcc1b6cc74adb65762c5e7a1b5a2b8df2ff
Heemank Verma <heemankv@gmail.com>
2022-01-17 16:05:30 +0530

    final

--- components/TimeLine/index.js
+++ components/TimeLine/index.js
@@ -10,80 +10,101 @@ import Image from "next/image";
 
 export default function TimeLine() {
   return (
-    <div className={styles.container} id="Timeline">
-      <div className={styles.innerContainer}>
-        <h1
-          style={{
-            textAlign: "center",
-            fontSize: "108px",
-            color: "white",
-            marginBottom: 10,
-            fontFamily: "chalkie2",
-            color: "#3ebb87",
-          }}
-        >
-          TIMELINE
-        </h1>
-        {/* <Image
-          src="/assets/underlines/2.png"
-          width="200"
-          height="20"
-          className={styles.imgLine}
-          alt="gayatri "
-        /> */}
-        <div className="timeline">
-          <div className="container right">
-            <div className="content">
-              <h1 style={{ fontFamily: "poppins" }}>2015</h1>
-              <p style={{ fontFamily: "poppins" }}>
-                The first Branch of Gayatri Institute opens in our own home,
-                thanks to the hardwork and tireless efforts of our founder, Mr.
-                Shubham Dubey.
-              </p>
+    <>
+      <h1
+        className=" onMobile headings"
+        style={{
+          textAlign: "center",
+          color: "white",
+          marginBottom: 10,
+          fontFamily: "chalkie2",
+          color: "#3ebb87",
+        }}
+      >
+        Our Journey
+      </h1>
+      <div className={styles.container} id="Timeline">
+        <div className={styles.innerContainer}>
+          <h1
+            className=" onDesktop headings"
+            style={{
+              textAlign: "center",
+              color: "white",
+              marginBottom: 10,
+              fontFamily: "chalkie2",
+              color: "#3ebb87",
+            }}
+          >
+            Our Journey
+          </h1>
+          {/* <Image
+      src="/assets/underlines/2.png"
+      width="200"
+      height="20"
+      className={styles.imgLine}
+      alt="gayatri "
+    /> */}
+          <div className="timeline">
+            <div className="container right">
+              <div className="content">
+                <h1
+                  style={{ fontFamily: "poppins", marginBottom: 0 }}
+                  className={styles.first}
+                >
+                  2015
+                </h1>
+                <p style={{ fontFamily: "poppins" }}>
+                  The first Branch of Gayatri Institute opens in our own home,
+                  thanks to the hardwork and tireless efforts of our founder,
+                  Mr. Shubham Dubey.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="container left">
-            <div className="content">
-              <h1 style={{ fontFamily: "poppins" }}>2017</h1>
-              <p style={{ fontFamily: "poppins" }}>
-                The second branch of Gayatri Institute and our dream Institute
-                centre opens up in Nawada Housing Complex as we expand for the
-                first time, spreading our wings further into the horizon.
-              </p>
+            <div className="container left">
+              <div className="content">
+                <h1 style={{ fontFamily: "poppins", marginBottom: 0 }}>2017</h1>
+                <p style={{ fontFamily: "poppins" }}>
+                  The second branch of Gayatri Institute and our dream Institute
+                  centre opens up in Nawada Housing Complex as we expand for the
+                  first time, spreading our wings further into the horizon.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="container right">
-            <div className="content">
-              <h1 style={{ fontFamily: "poppins" }}>2019</h1>
-              <p style={{ fontFamily: "poppins" }}>
-                We partner up with another like-minded individual, as Gayatri
-                Institute tests new but faithful waters in opening up a
-                franchise in Kiran Garden, with Mr. Prateek Goyal.
-              </p>
+            <div className="container right">
+              <div className="content">
+                <h1 style={{ fontFamily: "poppins", marginBottom: 0 }}>2019</h1>
+                <p style={{ fontFamily: "poppins" }}>
+                  We partner up with another like-minded individual, as Gayatri
+                  Institute tests new but faithful waters in opening up a
+                  franchise in Kiran Garden, with Mr. Prateek Goyal.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="container left">
-            <div className="content">
-              <h1 style={{ fontFamily: "poppins" }}>2020</h1>
-              <p style={{ fontFamily: "poppins" }}>
-                Another one of our Centre&apos;s branch open up in Dwarka Mor,
-                under the careful and valuable supervision of our founder Mr,
-                Shubham Dubey
-              </p>
+            <div className="container left">
+              <div className="content">
+                <h1 style={{ fontFamily: "poppins ", marginBottom: 0 }}>
+                  2020
+                </h1>
+                <p style={{ fontFamily: "poppins" }}>
+                  Another one of our Centre&apos;s branch open up in Dwarka Mor,
+                  under the careful and valuable supervision of our founder Mr,
+                  Shubham Dubey
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="container right">
-            <div className="content">
-              <h1 style={{ fontFamily: "poppins" }}>2021</h1>
-              <p style={{ fontFamily: "poppins" }}>
-                We partner up with another like-minded individual, as Gayatri
-                Institute opens up our newest franchise in Jain Colony Dwarka,
-                with Mr. Mishank Goyal.
-              </p>
+            <div className="container right">
+              <div className="content">
+                <h1 style={{ fontFamily: "poppins", marginBottom: 0 }}>2021</h1>
+                <p style={{ fontFamily: "poppins" }}>
+                  We partner up with another like-minded individual, as Gayatri
+                  Institute opens up our newest franchise in Jain Colony Dwarka,
+                  with Mr. Mishank Goyal.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
