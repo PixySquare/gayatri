420aed18ff7bb0171c8c6ca675c6741d80446ef8
Heemank Verma <heemankv@gmail.com>
2022-01-17 19:32:38 +0530

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import Head from "next/head";
-import Aos from "aos";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
-        <script src="bower_components/aos/dist/aos.js"></script>
       </Head>
       <Component {...pageProps} />
     </>
